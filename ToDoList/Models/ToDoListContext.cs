32e4c1b3cef7a71325f72c54dfb7e15ae290c3f8
Daniel Ras <ras98.daniel@gmail.com>
2020-05-22 00:00:41 +0300

    feat: add database context

---
+++ ToDoList/Models/ToDoListContext.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ToDoList.Models
+{
+    public class ToDoListContext : DbContext
+    {
+        public ToDoListContext(DbContextOptions<ToDoListContext> options) : base(options) { }
+        public DbSet<ToDoItem> ToDoItems { get; set; }
+    }
+}
