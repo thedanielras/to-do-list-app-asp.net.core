194b3906f273443d53f09c7ce7680de71aafbe6a
Daniel Ras <ras98.daniel@gmail.com>
2020-05-21 23:31:46 +0300

    feat: add ToDoItem model class

---
+++ ToDoList/Models/ToDoItem.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ToDoList.Models
+{
+    public class ToDoItem
+    {
+        public ToDoItem(string task)
+        {
+            Task = task;
+            Created = new DateTime();
+        }
+
+        public string Task { get; set; }
+        public DateTime Created { get; set; }
+        public bool Completed { get; set; }
+    }
+}
