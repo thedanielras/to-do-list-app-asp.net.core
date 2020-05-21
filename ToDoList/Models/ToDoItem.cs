8d114b1f77b785f85fba554b230a7a84c401ff1f
Daniel Ras <ras98.daniel@gmail.com>
2020-05-21 23:59:51 +0300

    feat: update ToDoItem model
    Add Id and Priority properties to ToDoItem model

--- ToDoList/Models/ToDoItem.cs
+++ ToDoList/Models/ToDoItem.cs
@@ -2,16 +2,25 @@
 
 namespace ToDoList.Models
 {
+    public enum Priority
+    {
+        Low,
+        Medium,
+        High
+    }
     public class ToDoItem
     {
-        public ToDoItem(string task)
+        public ToDoItem(string task, Priority priority)
         {
             Task = task;
             Created = new DateTime();
+            Priority = priority;
         }
 
+        public int Id { get; set; }
         public string Task { get; set; }
         public DateTime Created { get; set; }
         public bool Completed { get; set; }
+        public Priority Priority { get; set; }
     }
 }
