e849bb2e517d71857d26136ab015e1ff2d7ef3f0
Daniel Ras <ras98.daniel@gmail.com>
2020-05-22 11:23:47 +0300

    feat: add db seeder class

---
+++ ToDoList/Models/DbSeeder.cs
@@ -0,0 +1,15 @@
+﻿using System.Linq;
+using Microsoft.EntityFrameworkCore;
+namespace ToDoList.Models
+{
+    static class DbSeeder
+    {
+        public static void Seed(ToDoListContext context)
+        {
+            if (context.ToDoItems.Any())
+            {
+                context.ToDoItems.Add(new ToDoItem("Sample Task", Priority.Medium));
+            }
+        }
+    }
+}
