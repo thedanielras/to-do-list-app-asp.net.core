9427bdb4eb004d985559d788958f7657b8e6441e
Daniel Ras <ras98.daniel@gmail.com>
2020-05-26 03:00:14 +0300

    feat: update script logic

--- ToDoList/wwwroot/js/site.js
+++ ToDoList/wwwroot/js/site.js
@@ -1,4 +1,65 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿function ToDoListApp() {
+    let _this = this;
 
-// Write your JavaScript code.
+    let constructor = function () {
+        $(".btn_mark_completed").click(function () {
+            let toDoItemId = this.dataset.itemid;
+            doMarkCompletedRequest(toDoItemId);
+        });
+    }
+
+    let doMarkCompletedRequest = function (toDoItemId) {
+        $.ajax("/Home/MarkCompleted", {
+            data: "toDoItemId=" + toDoItemId,
+            method: "POST",
+            success: function (data, textStatus, jqXHR) {
+                console.log("Successfully marked as completed");
+                markCompleted(data);
+            },
+            error: function (jqXHR, textStatus, errorThrown) {
+                console.log("Failed requesting for item completion");
+                console.log("StatusCode: " + jqXHR.status);
+                console.log("Message: " + textStatus);
+            }
+        });
+    }
+
+    let markCompleted = function (todo) {
+        if (!todo || !todo.id) {
+            console.log("failed changing todo state");
+            return;
+        }
+
+        let toDoItemId = todo.id;
+        let toDoPageItem = $(".todo-container[data-itemid=" + toDoItemId + "]");
+
+        toDoPageItem
+            .removeClass("bg-dark")
+            .addClass("bg-info")
+            .find(".todo-container__task").css("text-decoration", "line-through");
+        toDoPageItem
+            .find("button.todo-container__action-button")
+            .unbind("click")
+            .click(function () {
+                alert("remove")
+            })
+            .removeClass("btn-primary")
+            .addClass("btn-danger")
+            .find("i.fa-check")
+            .removeClass("fa-check")
+            .addClass("fa-trash-alt");
+
+        if (todo.priority === 0) {
+            toDoPageItem
+                .find(".todo-container__priority")
+                .removeClass("text-info")
+                .addClass("text-light")
+        }
+    }
+
+    constructor();
+}
+
+$(document).ready(function () {
+    let toDoListApp = new ToDoListApp();
+});
